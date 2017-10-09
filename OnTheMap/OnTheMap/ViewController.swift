e06875d9966aa15e88ea8d815ba53ea09b66663f
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-09 23:29:50 +0800

    Added base project

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 09/10/17.
+//  Copyright © 2017 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
