4caa3b474f0935cd7c1acee0996f3111a66d4d65
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-23 21:55:45 +0800

    Added get user data after session

--- OnTheMap/OnTheMap/Controllers/Login/LoginViewController.swift
+++ OnTheMap/OnTheMap/Controllers/Login/LoginViewController.swift
@@ -31,8 +31,16 @@ class LoginViewController: UIViewController {
         if let email = emailTextField.text, let password = passwordTextField.text, email.isValidEmail(), password.characters.count > 0 {
             SessionClient.postSession(withEmail: email, password: password, completion: { (success) in
                 if success {
-                    self.performSegue(withIdentifier: "goMainStoryboard", sender: nil)
-                    print("success login")
+                    
+                    UserClient.getUserData(withId: MemoryStorage.shared.session?.account.id ?? "", completion: { (success) in
+                        if success {
+                            self.performSegue(withIdentifier: "goMainStoryboard", sender: nil)
+                            print("success login")
+                        } else {
+                            self.showAlert("Login Error", message: "Opsss, we got an error trying to fetch your user data.")
+                        }
+                    })
+                    
                 } else {
                     self.showAlert("Login Error", message: "Email or password does not match.")
                 }
