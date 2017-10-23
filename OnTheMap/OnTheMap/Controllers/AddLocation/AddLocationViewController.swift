ac6788957565a29465e216b686cd9e0f7dabcaa6
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-23 17:14:55 +0800

    Added forward geocode

--- OnTheMap/OnTheMap/Controllers/AddLocation/AddLocationViewController.swift
+++ OnTheMap/OnTheMap/Controllers/AddLocation/AddLocationViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import CoreLocation
 
 class AddLocationViewController: UIViewController {
 
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var linkTextField: UITextField!
+    
+    lazy var geocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,5 +34,52 @@ class AddLocationViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func findLocationPressed(_ sender: Any) {
+        getGeocoder()
+    }
+    
 
+}
+
+// MARK: - Geocoder actions
+
+extension AddLocationViewController {
+    func getGeocoder() {
+        
+        guard let location = locationTextField.text else {
+            // error
+            return
+        }
+        
+        guard let link = linkTextField.text else {
+            // error
+            return
+        }
+        
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            self.processResponse(withPlacemarks: placemarks, error: error)
+        }
+        
+    }
+    
+    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        
+        if let error = error {
+            print("Unable to Forward Geocode Address (\(error))")
+        } else {
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                let coordinate = location.coordinate
+                performSegue(withIdentifier: "showPostLocation", sender: nil)
+                print(coordinate)
+            } else {
+                // "No Matching Location Found"
+            }
+        }
+    }
 }
