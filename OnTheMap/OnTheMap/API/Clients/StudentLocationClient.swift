96bd538d6246622449e86685c0f08ca3af76fe94
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-23 15:51:51 +0800

    Added most recent order for students locations

--- OnTheMap/OnTheMap/API/Clients/StudentLocationClient.swift
+++ OnTheMap/OnTheMap/API/Clients/StudentLocationClient.swift
@@ -13,7 +13,7 @@ struct StudentLocationClient {
     static fileprivate let headers: [String: String] = ["X-Parse-Application-Id": "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", "X-Parse-REST-API-Key": "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"]
     
     static func getStudentsLocations(_ completion: @escaping (_ success: Bool) -> Void) {
-        let url = APIClient.buildURL(["limit": "100"] as [String: AnyObject], withHost: Constants.apiHostParseClasses, withPathExtension: Constants.apiPathUdacityParseStudentLocations)
+        let url = APIClient.buildURL(["limit": "100", "order": "-updatedAt"] as [String: AnyObject], withHost: Constants.apiHostParseClasses, withPathExtension: Constants.apiPathUdacityParseStudentLocations)
         
         _ = APIClient.performRequest(url, headerValues: headers, completion: { (dict, error) in
             
