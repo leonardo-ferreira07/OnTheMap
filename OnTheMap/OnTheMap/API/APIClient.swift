6ae62cbefa2952bd8826e1c66a6db928dceb0681
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-23 15:38:07 +0800

    Refactored APIClient

--- OnTheMap/OnTheMap/API/APIClient.swift
+++ OnTheMap/OnTheMap/API/APIClient.swift
@@ -32,9 +32,7 @@ class APIClient: NSObject {
                 return
             }
             
-            DispatchQueue.main.async(execute: {
-                completion(jsonObject, nil)
-            })
+            completion(jsonObject, nil)
             
         }, timeoutAfter: timeout) {
             
